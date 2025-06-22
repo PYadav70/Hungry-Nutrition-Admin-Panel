b226bcf3a659c4f055de357b6a04a85f1e6001aa
PYadav70 <technoyadav1234@gmail.com>
2025-06-22 23:56:26 +0530

    adding list api

--- src/pages/Add.jsx
+++ src/pages/Add.jsx
@@ -1,51 +1,123 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { assets } from '../assets/assets'
+import axios from 'axios'
+import { backendUrl } from '../App'
+import { toast } from 'react-toastify'
 
-const Add = () => {
+const Add = ({token}) => {
+
+  const [image1, setImage1] = useState(false)
+  const [image2, setImage2] = useState(false)
+  const [image3, setImage3] = useState(false)
+  const [image4, setImage4] = useState(false)
+
+  const [name,setName] = useState("")
+  const [description, setDescription] = useState("") 
+  const [price, setPrice] = useState("")
+  const [category, setCategory] = useState("Glutamine")
+  const [bestseller, setBestseller] = useState(false)
+
+  const onsubmitHandler = async (e)=>{
+     e.preventDefault();
+     try {
+      const formData = new FormData()
+
+      formData.append("name",name)
+      formData.append("description",description)
+      formData.append("price",price)
+      formData.append("category",category)
+      formData.append("bestseller",bestseller)
+
+      image1 && formData.append("image1",image1)
+      image2 && formData.append("image2",image2)
+      image3 && formData.append("image3",image3)
+      image4 && formData.append("image4",image4)
+
+      console.log("Token being sent:", token)
+
+
+      const response = await axios.post(backendUrl + "/api/product/add", formData,{headers:{token}})
+      console.log(response.data)
+
+      if(response.data.success){
+        toast.success(response.data.message)
+        setName('')
+          setDescription('')
+          setImage1('')
+          setImage2('')
+          setImage3('')
+          setImage4('')
+          setPrice('')
+        
+      }else{
+        toast.error(response.data.message)
+      }
+      
+     } catch (error) {
+      console.log(error)
+      toast.error(error.message)
+     }
+  }
   return (
-    <form className='flex flex-col w-full items-start gap-3'>
+    <form onSubmit={onsubmitHandler} className='flex flex-col w-full items-start gap-3'>
       <div>
         <p className='mb-2'>Upload Image</p>
         <div className='flex gap-2'>
           <label htmlFor="image1">
-            <img className='w-20' src={assets.upload_area} alt="" />
-            <input type="file" id="image1" hidden />
+            <img className='w-20' src={!image1 ? assets.upload_area : URL.createObjectURL(image1)} alt="" />
+            <input onChange={(e)=>setImage1(e.target.files[0])} type="file" id="image1" hidden />
           </label>
            <label htmlFor="image2">
-            <img className='w-20' src={assets.upload_area} alt="" />
-            <input type="file" id="image2" hidden />
+            <img className='w-20' src={!image2 ?assets.upload_area :  URL.createObjectURL(image2)} alt="" />
+            <input onChange={(e)=>setImage2(e.target.files[0])} type="file" id="image2" hidden />
           </label>
            <label htmlFor="image3">
-            <img className='w-20' src={assets.upload_area} alt="" />
-            <input type="file" id="image3" hidden />
+            <img className='w-20' src={!image3 ? assets.upload_area :  URL.createObjectURL(image3)} alt="" />
+            <input onChange={(e)=>setImage3(e.target.files[0])} type="file" id="image3" hidden />
           </label>
            <label htmlFor="image4">
-            <img className='w-20' src={assets.upload_area} alt="" />
-            <input type="file" id="image4" hidden />
+            <img className='w-20' src={!image4? assets.upload_area :  URL.createObjectURL(image4)} alt="" />
+            <input onChange={(e)=>setImage4(e.target.files[0])} type="file" id="image4" hidden />
           </label>
         </div>
       </div>
 
       <div className='w-full'>
         <p className='mb-2'>Product Name</p>
-        <input className='w-full max-w-[500px] px-3 py-2 ' type="text" placeholder='Type here' required />
+        <input onChange={(e)=>setName(e.target.value)} value={name} className='w-full max-w-[500px] px-3 py-2 ' type="text" placeholder='Type here' required />
       </div>
 
        <div className='w-full'>
         <p className='mb-2'>Product Description</p>
-        <textarea className='w-full max-w-[500px] px-3 py-2 ' type="text" placeholder='Write Description here' required />
+        <textarea onChange={(e)=>setDescription(e.target.value)} value={description} className='w-full max-w-[500px] px-3 py-2 ' type="text" placeholder='Write Description here' required />
       </div>
 
-      <div>
+      <div className='flex flex-col sm:flex-row gap-2 w-full sm:gap-8'>
         <div>
-          <p >Product Category</p>
-          <select >
-            <option value=""></option>
-            <option value=""></option>
-            <option value=""></option>
+          <p className='mb-2' >Product Category</p>
+          <select onChange={(e)=>setCategory(e.target.value)} value={category} className='w-full px-3 py-2' >
+            <option value="Glutamine">Glutamine</option>
+            <option value="Creatine">Creatine</option>
+            <option value="EAA+">EAA+</option>
+            <option value="Hungry Mass">Hungry Mass</option>
+            <option value="Pre Workout">Pre Workout</option>
+            <option value="Hungry Whey">Hungry Whey</option>
+            <option value="Hungry Multivitamin">Hungry Multivitamin</option>
+            <option value="Hungry Omega-3">Hungry Omega-3</option>
           </select>
         </div>
+
+        <div>
+          <p className='mb-2'>Product Price</p>
+          <input onChange={(e)=>setPrice(e.target.value)} value={price} className='w-full px-3 py-2 sm:w-[120px]' type="Number" placeholder='50' />
+        </div>
       </div>
+      <div className='flex gap-2 mt-2'>
+        <input onChange={(e)=>setBestseller(prev =>!prev)} checked={bestseller} type="checkbox" id='bestseller' />
+        <label className='cursor-pointer  ' htmlFor="bestseller"> Add to bestseller</label>
+      </div>
+
+      <button className='w-28 py-3 mt-4 bg-black text-white' type='submit'>ADD</button>
     </form>
   )
 }
